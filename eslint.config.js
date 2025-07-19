64a561f7efd8bd37035d70fb056c8d30de0ee1b5
yogami <gopal.yami@gmail.com>
2025-07-19 08:56:06 +0200

    eslint conflg on root location edited again

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,28 @@
-// ESLint v9+ config migrated from .eslintrc.json
-export default {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "no-unused-vars": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"]
+// ESLint v9+ flat config for your project
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        Node: "readonly",
+        module: "readonly",
+        require: "readonly",
+        process: "readonly"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin")
+    },
+    files: ["**/*.{js,ts,tsx,jsx}"],
+    ignores: ["node_modules/**", "dist/**"],
+    rules: {
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"]
+    }
   }
-};
+];
